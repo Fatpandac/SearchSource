75e1eab71d44d24a654f838e208f08c6e2f0093f
Fatpandac <i@fatpandac.com>
2025-12-03 22:09:02 +0800

    feat: add rspress

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,44 @@ import source from "./source";
 import { join } from "path";
 import { globSync } from "fast-glob";
 
-
 async function main() {
-    Object.entries(source).forEach(([docsName, data]) => {
-        const { repoPath, path } = data;
-        const docsIndex = Object.entries(path).reduce((prev, [locale, patterns]) => {
-            const path = join(__dirname, "..", repoPath);
-            const files = globSync(patterns, {
-                cwd: path,
-            });
+  Object.entries(source).forEach(([docsName, data]) => {
+    const { repoPath, path } = data;
+    const docsIndex = Object.entries(path).reduce(
+      (prev, [locale, patterns]) => {
+        const path = join(__dirname, "..", repoPath);
+        const files = globSync(patterns, {
+          cwd: path,
+        });
+        console.log(files, repoPath)
 
-            prev[locale] = files.map(file => join(path, file));
+        prev[locale] = files.map((file) => join(path, file));
 
-            return prev
-        }, {} as Record<string, string[]>);
+        return prev;
+      },
+      {} as Record<string, string[]>,
+    );
 
-        Object.entries(docsIndex).forEach(([locale, allFiles]) => {
-            console.log(`Generating search index for ${docsName} - ${locale} with ${allFiles.length} files.`);
-            const searchIndex = generateSearchIndex(allFiles);
-            const outputPath = join(__dirname, "..", "search-index", docsName, locale);
-            fs.mkdirSync(outputPath, { recursive: true });
-            fs.writeFileSync(join(outputPath, "search-index.json"), searchIndex, "utf-8");
-        })
+    Object.entries(docsIndex).forEach(([locale, allFiles]) => {
+      console.log(
+        `Generating search index for ${docsName} - ${locale} with ${allFiles.length} files.`,
+      );
+      const searchIndex = generateSearchIndex(allFiles);
+      const outputPath = join(
+        __dirname,
+        "..",
+        "search-index",
+        docsName,
+        locale,
+      );
+      fs.mkdirSync(outputPath, { recursive: true });
+      fs.writeFileSync(
+        join(outputPath, "search-index.json"),
+        searchIndex,
+        "utf-8",
+      );
     });
+  });
 }
-
 
 main();
--- src/source.ts
+++ src/source.ts
@@ -1,15 +1,25 @@
-const source = {
-    RSSHub: {
-        repoPath: "source/RSSHub-docs",
-        path: {
-            "zh-CN": "src/zh/**/*.md",
-            "en-US": [
-                "src/**/*.md",
-                "!src/zh/**"
-            ]
-        } as Record<string, string | string[]>
-    }
-} as const;
+type SourceInfo = {
+  repoPath: string;
+  path: Record<string, string | string[]>;
+};
 
+type Source = Record<"RSSHub" | "Rspress", SourceInfo>;
+
+const source: Source = {
+  RSSHub: {
+    repoPath: "source/RSSHub-docs",
+    path: {
+      "zh-CN": "src/zh/**/*.md",
+      "en-US": ["src/**/*.md", "!src/zh/**"],
+    },
+  },
+  Rspress: {
+    repoPath: "source/repress",
+    path: {
+      "zh-CN": "website/docs/zh/**/*.{md,mdx}",
+      "en-US": "website/docs/en/**/*.{md,mdx}",
+    }
+  }
+};
 
 export default source;
